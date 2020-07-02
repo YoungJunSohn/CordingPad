b939e3ce01157c7fd4560f5d6c6ebeb493cd1749
SohnYoungJun <sohnyj37@gmail.com>
2020-07-02 23:30:52 +0900

    [Add] 메모리 영역 구조도 추가

--- java-basic-concepts/src/main/referenceType/enumType/ChosenSeason.java
+++ java-basic-concepts/src/main/referenceType/enumType/ChosenSeason.java
@@ -6,7 +6,7 @@ public class ChosenSeason {
     public static void main(String[] args) {
 
         for(Season season : Season.values()){
-            System.out.println(season.getSeason());
+            System.out.println(season.getSeason()); //season에 나열된 문자 봄, 여름, 가을, 겨울이 출력된다.
         }
     }//main
 }//ChosenSeason
