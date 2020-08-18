ffd0d74697f0a135a88a6a7040861121d234ac54
SohnYoungJun <sohnyj37@gmail.com>
2020-08-18 22:42:50 +0900

    [Add] : <인터페이스>
    <내용>
    간단 예제 및 개념 요약
    <관련 이슈>

---
+++ java-basic-concepts/src/main/interfaces/ImplA.java
@@ -0,0 +1,6 @@
+package main.interfaces;
+
+public class ImplA implements InterfaceA {
+    public void s() {
+    }
+}//InterfaceA
---
+++ java-basic-concepts/src/main/interfaces/InterfaceA.java
@@ -0,0 +1,5 @@
+package main.interfaces;
+
+public interface InterfaceA {
+    public void s();
+}
