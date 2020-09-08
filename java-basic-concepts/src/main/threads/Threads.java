371d00cf0d83da3de46c3a148c9929099a53d4d5
SohnYoungJun <sohnyj37@gmail.com>
2020-09-08 22:09:23 +0900

    [Updatae] : <Thread>
    <내용>
    스레드의 개념정리 잘못된 것 수정 및 추가
    <관련 이슈>

--- java-basic-concepts/src/main/threads/Threads.java
+++ java-basic-concepts/src/main/threads/Threads.java
@@ -2,7 +2,7 @@ package main.threads;
 
 import java.awt.*;
 
-public class Threads {
+public class Threads extends Thread implements Runnable{
     /*자바의 모든 애플리케이션은 메인 스레드가 메인 메서드를 실행하면서 시작한다.
     메인 스레드는 main() 메서드의 첫 코드부터 아래로 순차적으로 실행하고, main()의 마지막 코드를 실행하거나 return 문을 만나면 실행이 종료된다.
     메인 스레드는 필요에 따라 작업 스레드들을 만들어서 병렬로 코드를 실행할 수 있다.
@@ -15,13 +15,23 @@ public class Threads {
 
     /*스레드의 생성
     스레드의 생성은 java.lang.Thread 클래스로부터 작업 스레드 객체를 직접 생성하려면
-    다음과 같이 Runnable을 매개 값으로 갖는 생성자를 호출해야 한다.
+    첫 번째로, 다음과 같이 Runnable을 매개 값으로 갖는 생성자를 호출하는 방법이 있다.
     Runnable은 인터페이스 타입의 객체인데 여기에는 run()이라는 메서드가 정의되어있으며, 구현 클래스는 run()을 오버라이딩해서
     작업 스레드가 실행할 코드를 작성해야 한다.
     Runnable은 작업 내용을 갖고 있는 객체일 뿐이지, 실제 스레드는 아니다. 때문에 구현 객체를 생성하고 나서
     이것을 매개값으로 해서 Thread 생성자를 호출해야 비로소 작업 스레드가 생성된다.
     * */
 
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    /*스레드의 생성2
+    * 두 번째 방법은 Thread 클래스를 상속받는 것이다.
+    * Thread 클래스는 내부에 run() 메서드를 갖고 있는데 이를 통해 실행할 서브 스레드를 만들수 있다.
+    * Thread 클래스를 상속받아 만든 run()은 Thread 클래스의 run() 메서드를 호출한다는 점에서 Runnable 인터페이스의 run()과 차이점이 있다.
+    * */
     public static void main(String[] args) {
         //메인메서드를 이용하여 메인스레드 !
 
